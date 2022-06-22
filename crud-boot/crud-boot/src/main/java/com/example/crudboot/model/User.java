039cf122ac2dffd20e90b773b26ff705e294579b
haracterwest <haracter.west@gmail.com>
2022-06-22 21:05:33 +0300

    Initial commit

---
+++ crud-boot/crud-boot/src/main/java/com/example/crudboot/model/User.java
@@ -0,0 +1,25 @@
+package com.example.crudboot.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+}
